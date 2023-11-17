de5869397a3128a62774f4eb75887901db0e58ad
RafaelRcruz <145867544+RafaelRcruz@users.noreply.github.com>
2023-11-17 13:32:35 -0300

    Add files via upload

---
+++ mock.js
@@ -0,0 +1,16 @@
+
+export const users = [
+    {
+        "id": 1,
+        "nome": "Usuário 1",
+        "email": "usuario1@email.com",
+        "senha": "senha123"
+    },
+    {
+        "id": 2,
+        "nome": "Usuário 2",
+        "email": "usuario2@email.com",
+        "senha": "senha456"
+    }
+];
+export const posts = [];
